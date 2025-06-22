94de370a65ec6f247cf624198d1e41560e10109f
ChioBlossom <rocioestefanycruces.a@gmail.com>
2025-06-21 23:52:29 -0400

    Add files via upload

--- Edutech-main/enrollment-service/src/main/java/com/edutech/enrollment_service/model/Enrollment.java
+++ Edutech-main/enrollment-service/src/main/java/com/edutech/enrollment_service/model/Enrollment.java
@@ -1,33 +1,10 @@
 package com.edutech.enrollment_service.model;
 
-import java.time.LocalDateTime;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@Entity
-@Table(name = "inscripciones")
-@NoArgsConstructor
-@AllArgsConstructor
-
 public class Enrollment {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @NotNull(message = "El ID de usuario no puede ser nulo")
     private Long userId;
-
-    @NotNull(message = "El ID de curso no puede ser nulo")
     private Long courseId;
-
-    private LocalDateTime enrollmentDate = LocalDateTime.now();
 }
