0a2bd037035c25994f348a1f9c1b86706514d39e
ChioBlossom <rocioestefanycruces.a@gmail.com>
2025-06-22 23:10:03 -0400

    hoy

--- Edutech-main/payment-service/src/main/java/com/edutech/payment_service/PaymentServiceApplication.java
+++ Edutech-main/payment-service/src/main/java/com/edutech/payment_service/PaymentServiceApplication.java
@@ -1,26 +1,13 @@
 package com.edutech.payment_service;
 
-import com.edutech.payment_service.model.Payment;
-import com.edutech.payment_service.service.PaymentService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class PaymentServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(PaymentServiceApplication.class, args);
 
-        PaymentService service = new PaymentService();
 
-        Payment payment = new Payment();
-        payment.setId(1L);
-        payment.setUser("Maria López");
-        payment.setAmount(14990);
-
-        service.processPayment(payment);
-        service.getAllPayments().forEach(p -> {
-            System.out.println("Pago de " + p.getUser() + ": $" + p.getAmount());
-        });
     }
 }
